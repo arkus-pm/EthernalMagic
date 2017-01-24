1cb5b1a35e45d8659f1b6d4715750ec82b0fd4b2
3D MEMES <intmaxfactor@gmail.com>
2017-01-24 09:08:28 +0200

    basics

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveScale;
+    public float jumpScale;
+    bool grounded;
+    // Use this for initialization
+    void Start () {
+        grounded = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A)) transform.Translate(Vector2.left * moveScale);
+        else if (Input.GetKey(KeyCode.D)) transform.Translate(Vector2.right * moveScale);
+        if (Input.GetKeyDown(KeyCode.W) && grounded) GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpScale, ForceMode2D.Impulse);
+        RaycastHit2D rayToGround = Physics2D.Raycast(transform.position, Vector2.down);
+        if (rayToGround.collider != null)
+        {
+            if (rayToGround.distance < 0.53) grounded = true; else grounded = false; 
+        }
+        Debug.Log(Mathf.Abs(rayToGround.distance) + ";" + grounded);
+    }
+}
